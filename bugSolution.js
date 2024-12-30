d20cdef31773eca63b3ff3f947b24211805edc66
BugHunter-X <askzenai@gmail.com>
2024-12-30 16:28:14 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,13 @@
+```javascript
+// pages/index.js
+import { StrictMode } from 'react';
+export default function Home() {
+  return (
+    <StrictMode>
+      <div>
+        <h1>Next.js 15 App</h1>
+      </div>
+    </StrictMode>
+  );
+}
+```
